7e5f7d1f2eb99f5b4772589e0d2f7007c6ddf0a4
Daniel Faure <faure.daniel.57@gmail.com>
2019-12-27 12:45:45 -0300

    parte de lo que vimos el 20.12.19
    Subidos sin testear, tal como estaban

---
+++ ej0.php
@@ -0,0 +1,23 @@
+<?php
+
+// no te olvides que hacemos esto por gusto
+
+/*
+aca ponemos todo lo necesario para recordar que es esto
+*/
+
+$resultado=5+3;
+print $resultado."\n" ;
+
+echo $resultado."\n";
+
+
+
+
+
+print ("\nTermino\n");
+
+?>
+   
+
+
---
+++ ej0.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+resultado = 34 if (5>13) else 9
+
+print(resultado)
+# aca ponemos todo lo necesario para recordar que es esto
+
+
+print ("Terminó de codear\n")
